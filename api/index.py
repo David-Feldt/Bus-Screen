8ee33950ed51d7641c291eb2adb2cf5edbfd3440
David Feldt <dsfeldt@gmail.com>
2024-01-27 12:11:28 -0500

    quick html change

--- api/index.py
+++ api/index.py
@@ -158,9 +158,11 @@ class MyHandler(BaseHTTPRequestHandler):
         
         html_content = f"""
         <!DOCTYPE html>
-        <html>
+        <html lang='en'>
         <head>
             <title>Next Bus</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="refresh" content="5">
         </head>
         <body>
